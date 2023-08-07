0593f56ec1887f791a7268bacfef466800052288
unknown <devincalimto@gmail.com>
2023-08-07 08:47:44 +0700

    Setup welcome message

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { REACT_APP_CALCULATOR_API_BASE_URL } from "../apis/services";
+import { io } from "socket.io-client";
+
+const socket = io(REACT_APP_CALCULATOR_API_BASE_URL as string);
 
 const Chat = () => {
-  return <div>Chat</div>;
+    const navigate = useNavigate();
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        const userID = sessionStorage.getItem("userID");
+        if (!userID) {
+            navigate("/");
+        } else {
+            socket.emit("join", { userID, socketID: socket.id });
+        }
+    }, []);
+    useEffect(() => {
+        socket.on("previousMessage", (data) => {
+            setMessages(data);
+        });
+    }, [socket]);
+    console.log("messages", messages);
+    return <div>Chat</div>;
 };
 
 export default Chat;
